ef24667e14e34d04be761c17126fd809394bd68c
Peter T. Winkler <peter.winkler@code.berlin>
2021-12-01 19:07:22 +0100

    updated README.md to be more general
    Signed-off-by: Peter T. Winkler <peter.winkler@code.berlin>

--- bin/se_01_assessment_problem_01.dart
+++ bin/se_01_assessment_problem_01.dart
@@ -1,4 +1,8 @@
 void main(List<String> arguments) {
+  mineSweeper();
+}
+
+void mineSweeper() {
   /// The input formatted as a matrix.
   final inputMatrix3x3 = [
     ['.', '.', '*'],
@@ -18,11 +22,12 @@ void main(List<String> arguments) {
   int rowsCount;
   int columnsCount;
 
-
   /// This matrix will be modified to give the desired input.
   /// The items are dynamic, so that '*' Strings marking the mines can
   /// overwrite the integer values.
   List<List<dynamic>> outputMatrix;
+
+  List<List<dynamic>> playingField;
 
   /// Counts the number of rows and columns of the matrix. Assumes that
   /// all rows have the same number of columns.
@@ -31,11 +36,22 @@ void main(List<String> arguments) {
     columnsCount = inputMatrix[0].length;
   }
 
-  void generateOutputMatrix(rowsCount, columnsCount) {
-
+  /// Generates the output matrix based on the number of rows and columns given.
+  /// Fills it up with the value passed to the 'filler' argument.
+  void generateSizedMatrixOf(rowsCount, columnsCount, filler) {
     outputMatrix = List.empty(growable: true);
-    for (var i = 0; i < rowsCount; i++){
-        outputMatrix.add(List.filled(columnsCount, 0));
+    for (var i = 0; i < rowsCount; i++) {
+      outputMatrix.add(List.filled(columnsCount, filler));
+    }
+  }
+
+  /// Generates the playing field matrix based on the number of rows and
+  /// columns given.
+  /// Fills it up with the value passed to the 'filler' argument.
+  void generateBlankPlayingField(rowsCount, columnsCount, filler) {
+    playingField = List.empty(growable: true);
+    for (var i = 0; i < rowsCount; i++) {
+      playingField.add(List.filled(columnsCount, filler));
     }
   }
 
@@ -43,7 +59,6 @@ void main(List<String> arguments) {
   /// then adds 1 to the value of all existing neighbouring fields
   /// in the output matrix.
   void add1ToNeighbors(int i, int j) {
-
     /// Marks the location of the mine in the output matrix.
     outputMatrix[i][j] = '*';
 
@@ -54,29 +69,27 @@ void main(List<String> arguments) {
         /// Checks if the coordinates point to an existing field,
         /// and if it is of type integer.
         /// Skips the middle field, because it is now a String.
-        if ((i+k >= 0 && i+k < rowsCount) &&
-            (j+l >= 0 && j+l < columnsCount)) {
-          if (outputMatrix[i+k][j+l] is int) {
-            outputMatrix[i+k][j+l]++;
+        if ((i + k >= 0 && i + k < rowsCount) &&
+            (j + l >= 0 && j + l < columnsCount)) {
+          if (outputMatrix[i + k][j + l] is int) {
+            outputMatrix[i + k][j + l]++;
           }
         }
       }
     }
   }
 
-  List mineSweeper(inputMatrix) {
+  List generateMinefieldData(inputMatrix) {
     /// Counts the rows and columns of the input matrix and
     /// writes them in the global rowsCount and columnsCount variables.
     countRowsAndColumns(inputMatrix);
 
-    generateOutputMatrix(rowsCount, columnsCount);
-
-
+    generateSizedMatrixOf(rowsCount, columnsCount, 0);
 
     /// Iterates through input matrix to find mines and modifies
     /// the output matrix accordingly.
     for (var i = 0; i < rowsCount; i++) {
-      for(var j = 0; j < columnsCount; j++) {
+      for (var j = 0; j < columnsCount; j++) {
         if (inputMatrix[i][j] == '*') {
           add1ToNeighbors(i, j);
         }
@@ -96,9 +109,24 @@ void main(List<String> arguments) {
     }
   }
 
-  /// Calls the mineSweeper function to get the output.
-  var output = mineSweeper(inputMatrix4x4);
+  void gameLoop() {
+    bool isGameOn = true;
+
+    while (isGameOn) {
+      print(playingField);
+      printMatrixToConsole(playingField);
+    }
+  }
+
+
+  /// Calls the generateMinefieldData function to get the minefield information
+  /// that the player needs to uncover.
+  var minefieldData = generateMinefieldData(inputMatrix4x4);
 
   /// Prints output to the console.
-  printMatrixToConsole(output);
+  //printMatrixToConsole(minefieldData);
+
+  generateBlankPlayingField(rowsCount, columnsCount, '-');
+
+  gameLoop();
 }
